d9fbaeace9e706911e3334cc6a344400e13a27ca
kike <ebellidoperejil2@gmail.com>
2023-09-28 17:06:27 +0200

    commit de prueba

--- Listado_ejercicios_objetos_01_EnriqueBellido/Contador/src/Contador/maincontador.java
+++ Listado_ejercicios_objetos_01_EnriqueBellido/Contador/src/Contador/maincontador.java
@@ -1,7 +1,7 @@
 package Contador;
 import java.util.*;
 public class maincontador {
-
+//prueba
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in); 
